a6ce1b793f08d55ed1deedc92d1ca70939595a8b
Sanaia <sgpierre2003@gmail.com>
2025-10-15 22:23:40 -0700

    working on making the separate blog posts pages

--- src/blog.js
+++ src/blog.js
@@ -1,19 +1,35 @@
 var blogs = [
     {
-        title: "Sanaia at Work",
-        date: "08-27-2025",
-        description: "I’m happy to share that I’m starting a new position as the Programming Director for the Cal Poly Black Student Union!",
-        image: "./images/placeholder.png",
+        title: "2025-26 SY Hack4Impact",
+        date: "10-06-2025",
+        description: "I’m happy to share that I’m getting started with Hack4Impact as a participant in their Bootcamp Project!",
+        image: "./images/hack-4-impact.png",
         imageAlt: "Placeholder Image",
-        slug: "get-to-my-image",
+        slug: "hack-4-impact-bootcamp",
     },
     {
-        title: "Sanaia in Class",
+        title: "2025-26 SY CSC 402",
+        date: "09-18-2025",
+        description: "I’m happy to share that I’m getting started with my Capstone Project with PG&E!",
+        image: "./images/software-engineering.png",
+        imageAlt: "CSC 402",
+        slug: "pg-&-e-capstone",
+    },
+    {
+        title: "2025-26 SY BSU E-Board",
+        date: "08-27-2025",
+        description: "I’m happy to share that I’m starting a new position as the Programming Director for the Cal Poly Black Student Union!",
+        image: "./images/bsu.PNG",
+        imageAlt: "Cal Poly BSU",
+        slug: "cal-poly-bsu",
+    },
+    {
+        title: "2025-26 SY Job Position",
         date: "07-01-2025",
         description: "I’m happy to share that I’m starting a new position as a Student Web Developer at Cal Poly Office of Academic Innovations & Planning!",
-        image: "./images/placeholder.png",
-        imageAlt: "Placeholder Image",
-        slug: "get-to-my-image",
+        image: "./images/aip.jpg",
+        imageAlt: "Academic Innovations & Programs",
+        slug: "student-web-developer",
     },
 ];
 var blogContainer = document.getElementById("blog-container");
@@ -33,10 +49,13 @@ blogs.forEach(function (blog) {
     date.textContent = blog.date;
     var description = document.createElement("p");
     description.textContent = blog.description;
+    var blogLink = document.createElement("a");
+    blogLink.href = "/blog/".concat(blog.slug, ".html");
     if (blogContainer) {
         blogContainer.append(containerDiv);
     }
     imgDiv.append(image);
     textDiv.append(title, date, description);
     containerDiv.append(imgDiv, textDiv);
+    blogLink.append(containerDiv);
 });
--- src/blog.ts
+++ src/blog.ts
@@ -9,22 +9,40 @@ type Blog = {
 
 const blogs: Blog[] = [
   {
-    title: "Sanaia at Work",
+    title: "2025-26 SY Hack4Impact",
+    date: "10-06-2025",
+    description:
+      "I’m happy to share that I’m getting started with Hack4Impact as a participant in their Bootcamp Project!",
+    image: "./images/hack-4-impact.png",
+    imageAlt: "Placeholder Image",
+    slug: "hack-4-impact-bootcamp",
+  },
+  {
+    title: "2025-26 SY CSC 402",
+    date: "09-18-2025",
+    description:
+      "I’m happy to share that I’m getting started with my Capstone Project with PG&E!",
+    image: "./images/software-engineering.png",
+    imageAlt: "CSC 402",
+    slug: "pg-&-e-capstone",
+  },
+  {
+    title: "2025-26 SY BSU E-Board",
     date: "08-27-2025",
     description:
       "I’m happy to share that I’m starting a new position as the Programming Director for the Cal Poly Black Student Union!",
-    image: "./images/placeholder.png",
-    imageAlt: "Placeholder Image",
-    slug: "get-to-my-image",
+    image: "./images/bsu.PNG",
+    imageAlt: "Cal Poly BSU",
+    slug: "cal-poly-bsu",
   },
   {
-    title: "Sanaia in Class",
+    title: "2025-26 SY Job Position",
     date: "07-01-2025",
     description:
       "I’m happy to share that I’m starting a new position as a Student Web Developer at Cal Poly Office of Academic Innovations & Planning!",
-    image: "./images/placeholder.png",
-    imageAlt: "Placeholder Image",
-    slug: "get-to-my-image",
+    image: "./images/aip.jpg",
+    imageAlt: "Academic Innovations & Programs",
+    slug: "student-web-developer",
   },
 ];
 
@@ -53,10 +71,14 @@ blogs.forEach((blog) => {
   const description = document.createElement("p");
   description.textContent = blog.description;
 
+  const blogLink = document.createElement("a");
+  blogLink.href = `/blog/${blog.slug}.html`;
+
   if (blogContainer) {
     blogContainer.append(containerDiv);
   }
   imgDiv.append(image);
   textDiv.append(title, date, description);
   containerDiv.append(imgDiv, textDiv);
+  blogLink.append(containerDiv);
 });
