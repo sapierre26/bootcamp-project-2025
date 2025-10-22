9cbf34b54cdbdda7c7c11b41dec57e7495b30fed
Sanaia <sgpierre2003@gmail.com>
2025-10-21 22:15:14 -0700

    completed the 'Organizing your Data', 'Planning Components', the first step of the 'Completing the Frontend' section

---
+++ bootcamp-milestone-2/app/blogData.ts
@@ -0,0 +1,49 @@
+export interface Blog {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+}
+
+const blogs: Blog[] = [
+  {
+    title: "2025-26 SY Hack4Impact",
+    date: "10-06-2025",
+    description:
+      "I’m happy to share that I’m getting started with Hack4Impact as a participant in their Bootcamp Project!",
+    image: "./images/hack-4-impact.png",
+    imageAlt: "Placeholder Image",
+    slug: "hack-4-impact-bootcamp",
+  },
+  {
+    title: "2025-26 SY CSC 402",
+    date: "09-18-2025",
+    description:
+      "I’m happy to share that I’m getting started with my Capstone Project with PG&E!",
+    image: "./images/software-engineering.png",
+    imageAlt: "CSC 402",
+    slug: "pg-&-e-capstone",
+  },
+  {
+    title: "2025-26 SY BSU E-Board",
+    date: "08-27-2025",
+    description:
+      "I’m happy to share that I’m starting a new position as the Programming Director for the Cal Poly Black Student Union!",
+    image: "./images/bsu.PNG",
+    imageAlt: "Cal Poly BSU",
+    slug: "cal-poly-bsu",
+  },
+  {
+    title: "2025-26 SY Job Position",
+    date: "07-01-2025",
+    description:
+      "I’m happy to share that I’m starting a new position as a Student Web Developer at Cal Poly Office of Academic Innovations & Planning!",
+    image: "./images/aip.jpg",
+    imageAlt: "Academic Innovations & Programs",
+    slug: "student-web-developer",
+  },
+];
+
+export default blogs;
---
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./navbar.module.css";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    // replace everything in between the <header> & <header /> tags
+    // with your navbar code from your earlier milestones
+    // NOTE: we use "class" in HTML but React is quirky so we have to 
+    // change all the "class" to "className"
+    <header className={style.navbar}>
+      <h1 className="logo"> Sanaia's Personal Website </h1>
+      <nav className="nav-list">
+        {/* We'll use Link from now on instead of <a></a>
+			      Links are just Next.js wrapper arounds <a> elements anyways
+         */}
+        <Link className="nav-list-item" href="/">Home</Link>
+        <Link className="nav-list-item" href="/blogs">Sanaia's Blog</Link>
+        <Link className="nav-list-item" href="/portfolio">Sanaia's Portfolio</Link>
+        <Link className="nav-list-item" href="/resume">Sanaia's Resume</Link>
+        <Link className="nav-list-item" href="/about">Contact Me</Link>
+				{/* More Links ... */}
+      </nav>
+    </header>
+  );
+}
