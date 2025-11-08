3d89b62471401491c13c4877955b9394f525aed1
Sanaia <sgpierre2003@gmail.com>
2025-11-07 20:48:12 -0800

    working on milestones

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     // replace everything in between the <header> & <header /> tags
     // with your navbar code from your earlier milestones
-    // NOTE: we use "class" in HTML but React is quirky so we have to 
+    // NOTE: we use "class" in HTML but React is quirky so we have to
     // change all the "class" to "className"
     <header className={style.navbar}>
       <h1 className="logo"> Sanaia's Personal Website </h1>
@@ -14,11 +14,21 @@ export default function Navbar() {
         {/* We'll use Link from now on instead of <a></a>
 			      Links are just Next.js wrapper arounds <a> elements anyways
          */}
-        <Link className="nav-list-item" href="/">Home</Link>
-        <Link className="nav-list-item" href="/blogs">Sanaia's Blog</Link>
-        <Link className="nav-list-item" href="/portfolio">Sanaia's Portfolio</Link>
-        <Link className="nav-list-item" href="/resume">Sanaia's Resume</Link>
-        <Link className="nav-list-item" href="/about">Contact Me</Link>
+        <Link className="nav-list-item" href="/">
+          Home
+        </Link>
+        <Link className="nav-list-item" href="/blog">
+          Sanaia's Blog
+        </Link>
+        <Link className="nav-list-item" href="/portfolio">
+          Sanaia's Portfolio
+        </Link>
+        <Link className="nav-list-item" href="/resume">
+          Sanaia's Resume
+        </Link>
+        <Link className="nav-list-item" href="/about">
+          Contact Me
+        </Link>
       </nav>
     </header>
   );
